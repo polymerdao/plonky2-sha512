285205184bcae8960606d2397e343ee92631bc3e
Sai Deng <dengsai@hotmail.com>
2022-06-23 16:55:54 -0700

    Add benchmark functions

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ```console
 cargo run --release --package plonky2_sha512 --bin plonky2_sha512
 ```
+
+Benchmark on Macbook Pro M1
+
+```console
+    Finished release [optimized] target(s) in 0.03s
+     Running `target/release/plonky2_sha512`
+Constructing inner proof with 40830 gates
+[INFO  plonky2::plonk::circuit_builder] Degree before blinding & padding: 40921
+[INFO  plonky2::plonk::circuit_builder] Degree after blinding & padding: 65536
+[DEBUG plonky2::plonk::circuit_builder] Building circuit took 3.5057101s
+[DEBUG plonky2::util::timing] 5.7480s to prove
+[DEBUG plonky2::util::timing] 0.0065s to verify
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::Result;
+use log::{Level, LevelFilter};
 use plonky2::iop::witness::{PartialWitness, Witness};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::util::timing::TimingTree;
 use plonky2_sha512::circuit::{array_to_bits, make_circuits};
 use sha2::{Digest, Sha512};
 
@@ -34,15 +36,32 @@ pub fn prove_sha512(msg: &[u8]) -> Result<()> {
         }
     }
 
+    println!(
+        "Constructing inner proof with {} gates",
+        builder.num_gates()
+    );
     let data = builder.build::<C>();
+    let timing = TimingTree::new("prove", Level::Debug);
     let proof = data.prove(pw).unwrap();
+    timing.print();
 
-    data.verify(proof)
+    let timing = TimingTree::new("verify", Level::Debug);
+    let res = data.verify(proof);
+    timing.print();
+
+    res
 }
 
 fn main() -> Result<()> {
-    let mut msg = vec![0; 128 as usize];
-    for i in 0..127 {
+    // Initialize logging
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp(None);
+    builder.filter_level(LevelFilter::Debug);
+    builder.try_init()?;
+
+    const MSG_SIZE: usize = 128;
+    let mut msg = vec![0; MSG_SIZE as usize];
+    for i in 0..MSG_SIZE - 1 {
         msg[i] = i as u8;
     }
     prove_sha512(&msg)
